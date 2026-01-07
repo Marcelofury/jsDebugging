f88bda9ee023985ad7ac54ca71953245e624513e
Marcelofury <marcelofury13579@gmail.com>
2026-01-07 09:34:04 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let username = 'Butera Marcel';
+
+let intructor = 'Gorret';
+
+let course = 'Javascript Debugging';
+
+console.log(`Hello ${username}, welcome to the ${course} course with ${intructor} as your instructor!`);
+
+//console.error('This is an error message for debugging purposes.');
+let age = 25;
+
+if (age >= 18) {   
+    console.error('You are old enough to vote!');
+}
+else {
+    console.error('You are not old enough to vote yet.');
+}
+
+
+
